4fe1e1e6b8555a3a27558242cf99c4b15c9e1ab3
ethwillz <ewwilliams6@gmail.com>
2016-09-07 15:58:44 -0500

    Added implementation of text to speech for iOS

--- Hello/Hello.iOS/TextToSpeechImplementation.cs
+++ Hello/Hello.iOS/TextToSpeechImplementation.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AVFoundation;
+using Hello.iOS;
+using System;
 
+[assembly: Xamarin.Forms.Dependency(typeof(TextToSpeechImplementation))]
 namespace Hello.iOS
 {
-    class TextToSpeechImplementation
+    class TextToSpeechImplementation : TextToSpeechInterface
     {
+        public void toSpeech(String sentence)
+        {
+            var speechSythensizer = new AVSpeechSynthesizer();
+
+            var speechUtterance = new AVSpeechUtterance(sentence)
+            {
+                Rate = AVSpeechUtterance.MaximumSpeechRate / 4,
+                Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
+                Volume = 0.5f,
+                PitchMultiplier = 1.0f
+            };
+
+            speechSythensizer.SpeakUtterance(speechUtterance);
+        }
     }
 }
